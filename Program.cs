343053ed79c758d1eb38aa53aba5a7cffd26af33
Илья Аникин <mercury2612198@gmail.com>
2021-07-16 07:51:45 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace VeeamTaskKiller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TaskKiller task = new TaskKiller();
+            task.StartKiller(args[0], args[1], args[2]);
+        }
+    }
+}
---
+++ TaskKiller.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace VeeamTaskKiller
+{
+    class TaskKiller
+    {
+        public string ProcessName { get; set; }
+        public int KillTime { get; set; }
+        public int CheckTime { get; set; }
+        Timer killTimer;
+        public void StartKiller(string process, string killT, string checkT)
+        {
+            ProcessName = process.Replace(".exe", "");
+            KillTime = int.Parse(killT) * 60000;
+            CheckTime = int.Parse(checkT) * 60000;
+            killTimer = new(KillProcess, null, 0, CheckTime);
+            HoldOn();
+        }
+        public void KillProcess(object state)
+        {
+            Process[] curProcess = Process.GetProcessesByName(ProcessName);
+            if (curProcess.Length != 0)
+            {
+                foreach (var i in curProcess)
+                {
+                    Console.WriteLine($"Process: {i.ProcessName}({i.Id})\nStartup: {i.StartTime}\nLifetime: {DateTime.Now.Subtract(i.StartTime).ToString(@"hh\:mm\:ss")}\n");
+                    if ((DateTime.Now.Subtract(i.StartTime)).TotalMilliseconds > KillTime)
+                    {
+                        i.Kill();
+                        Console.WriteLine("Process sucsesful killed");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{ProcessName} lifetime less than {KillTime / 60000} minutes");
+                    }
+                }
+            }
+            else
+                Console.WriteLine($"{ProcessName} not found");
+        }
+        public void HoldOn()
+        {
+            Console.ReadLine();
+        }
+    }
+}
